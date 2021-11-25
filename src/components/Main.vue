f6c601e4bcdac3f87c6cb24b488d96eb1b7df038
andreafontaneto <andrea.fontaneto@gmail.com>
2021-11-25 12:11:21 +0100

    card structure

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,101 @@
 <template>
   <main>
-    <div class="container">
-      CONTENT GOES HERE
+    
+    <div class="jumbotron">
+      <img src="../assets/img/jumbotron.jpg" alt="jumbotron">
     </div>
+    
+    <div class="container">
+      
+      <div class="current-series">
+        <h5>CURRENT SERIES</h5>
+      </div>
+
+      <div class="cards-box">
+        
+        <div class="card">
+          
+          <div class="card-img">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="x">
+          </div>
+
+          <div class="card-title">
+            <p>NOME CARD</p>
+          </div>
+          
+        </div>
+        <div class="card">
+          
+          <div class="card-img">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="x">
+          </div>
+
+          <div class="card-title">
+            <p>NOME CARD</p>
+          </div>
+          
+        </div>
+        <div class="card">
+          
+          <div class="card-img">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="x">
+          </div>
+
+          <div class="card-title">
+            <p>NOME CARD</p>
+          </div>
+          
+        </div>
+        <div class="card">
+          
+          <div class="card-img">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="x">
+          </div>
+
+          <div class="card-title">
+            <p>NOME CARD</p>
+          </div>
+          
+        </div>
+        <div class="card">
+          
+          <div class="card-img">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="x">
+          </div>
+
+          <div class="card-title">
+            <p>NOME CARD</p>
+          </div>
+          
+        </div>
+        <div class="card">
+          
+          <div class="card-img">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="x">
+          </div>
+
+          <div class="card-title">
+            <p>NOME CARD</p>
+          </div>
+          
+        </div>
+        <div class="card">
+          
+          <div class="card-img">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="x">
+          </div>
+
+          <div class="card-title">
+            <p>NOME CARD</p>
+          </div>
+          
+        </div>
+
+      </div>
+
+
+    </div>
+  
   </main>
 </template>
 
@@ -14,11 +107,54 @@ export default {
 
 <style lang="scss" scoped>
 
+  @import "../assets/style/vars.scss";
+
   main {
     background-color: #151515;
     color: white;
     font-size: 30px;
-    padding: 30px 0;
+
+    .jumbotron {
+      height: 400px;
+      background-color: cornflowerblue;
+      overflow: hidden;
+    }
+
+    .current-series{
+      width: 220px;
+      padding: 20px;
+      font-size: 25px;
+      color: white;
+      background-color: $blue-color;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .cards-box{
+      display: flex;
+      flex-wrap: wrap;
+
+      .card{
+        width: calc((100% / 6) - 20px);
+        margin: 20px;
+
+        .card-img{
+          width: 100%;
+          height: 180px;
+          overflow: hidden;
+        }
+
+        .card-title p{
+          font-size: 14px;
+          color: grey;
+          margin: 10px 0;
+        }
+        
+      }
+    }
+
+    
   }
 
 </style>
